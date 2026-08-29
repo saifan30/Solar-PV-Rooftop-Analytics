7bacd999c9cc2b2641069905e4c6b228da9c0b57
Saifan Khalfe <khalfesaifan333@gmail.com>
2026-08-29 15:18:47 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Solar-PV-Rooftop-Analytics/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
